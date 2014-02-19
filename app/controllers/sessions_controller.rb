093af9f74aa0a45a89d9a44cdfc4f4b34319a31a
sanjayongraph <cs.sanjayyadav@outlook.com>
2014-02-19 19:51:06 +0530

    updating and showing users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,25 @@
 class SessionsController < ApplicationController
-	def new
-	end
+   def new
+   end
 
-	def create
-  user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
-    else
+   def create
+     user = User.find_by(email: params[:session][:email].downcase)
+     if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to root_url
+
+    # Sign the user in and redirect to the user's show page.
+     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
-	end
-end
-	def destroy
-	end
+     end
+    end
+  
+   def destroy
+    logger.debug "11111111111"
+    sign_out
+    logger.debug "22222222"
+    redirect_to root_url
+    logger.debug "33333333"
+   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140218083516) do
+ActiveRecord::Schema.define(version: 20140218131500) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140218083516) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_token"
+    t.string   "password_digest"
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
